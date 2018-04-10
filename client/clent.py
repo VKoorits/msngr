b6c8883dd0de6070cead84c725a57ffbb7387ec5
VKoorits <koorits.viktor@yandex.ru>
2018-04-10 20:25:55 +0300

    integrated headers in every package of data

--- client/clent.py
+++ client/clent.py
@@ -1,48 +1,136 @@
 import socket
 
+
 RANDOM_TEXT_SIZE = 16   # in server_ex.py
 TOKEN_SIZE = 8          # in server_ex.py
+ANSWER_SIZE = 255
 LOGIN = "barakuda"
+DELEMITER = ":"
+OK_ANSWER = "ok"
+OK_CODE = 0
+SERVER_HEADER_SIZE = 5
+CLIENT_HEADER_SIZE = 4
+
+p = print
+# p     - debug
+# print - interface
+
+def recvData(sock):
+    header = sock.recv(SERVER_HEADER_SIZE)
+    data_size = int.from_bytes(header[0:4], byteorder='little')
+    code = int.from_bytes(header[4:5], byteorder='little')
+
+    data = sock.recv(data_size)
+    return data, code
+
+def sendDataB(sock, data, datatype):
+    req = (len(data)).to_bytes(4, byteorder='little') + data
+    sock.send(req)
+
+def sendData(sock, data, datatype):
+    sendDataB(sock, bytes(data, "utf-8"), datatype)
+
 
 def connect():
     sock = socket.socket()
     sock.connect(('localhost', 5001))
     return sock
 
-def login(sock, name):
-    req = "GET_TKN:" + name
+def login(sock, login):
+    req = ":".join(["GET_TKN", login])
+    sendData(sock, req, datatype="cmd")
 
-    sock.send(bytes(req, "utf-8"))
-    rand_text = sock.recv(RANDOM_TEXT_SIZE)
+    rand_text, code = recvData(sock)
+    if code != OK_CODE:
+        p(rand_text, code)
+        return
     # DECRYPT
-    sock.send(rand_text)
-    token = sock.recv(TOKEN_SIZE).decode("utf-8")
-    print(token)
+    #rand_text = b"popa"
+    sendDataB(sock, rand_text, datatype="data")
+
+
+
+    token, code = recvData(sock)
+    token = token.decode("utf-8")
+    if code != OK_CODE:
+        p(token, code)
+        return
+
     if token == '':
-        raise ValueError("INIT ERROR")
+        raise ValueError("INIT ERROR, empty token")
+
+    res, code = recvData(sock)
+    res = res.decode("utf-8")
+    if res != OK_ANSWER or code != OK_CODE:
+        raise ValueError(res)
+    p("LOGIN:\t\t" + res)
     return token
 
 def quit(sock, login, token):
-    req = "QUIT:" + login + ";" + token
-    print(req)
-    sock.send(bytes(req, "utf-8"))
+    req = ":".join(["QUIT", login, token])
+    sendData(sock, req, datatype="cmd")
+
+    res, code = recvData(sock)
+    res = res.decode("utf-8")
+
+    if res != OK_ANSWER or code != OK_CODE:
+        raise ValueError(res)
+
+    p("QUIT:\t\t" + res)
+
+def send_msg(sock, login, token):
+    msg = input().rstrip()
+    getter = input().rstrip()
+    req  = ":".join(["SEND_MSG", login, token, getter, msg])
+    sock.send( bytes(req, "utf-8") )
+    res, code = recvData(sock)
+    res = res.decode("utf-8")
+
+    if res != OK_ANSWER or code != OK_CODE:
+        raise ValueError(res)
+
+    p("SEND:\t\t" + res)
+
+def get_new_msg(sock, login, token):
+    req = ":".join(["GET_MSG", login, token])
+    sendData(sock, req, datatype="cmd")
+
+
+    res, code = recvData(sock)
+    res = res.decode("utf-8")
+
+    if res != OK_ANSWER or code != OK_CODE:
+        raise ValueError(res)
+
+    p("GET_MSG:\t" + res)
+
+def test(sock, login, token):
+    req = ":".join(["TEST", login, token])
+    sendData(sock, req, datatype="cmd")
+
+
+    res, code = recvData(sock)
+    res = res.decode("utf-8")
+
+    if res != OK_ANSWER or code != OK_CODE:
+        raise ValueError(res)
+
+    p("TEST:\t\t" + res)
 
 
 ##################################
 sock = connect()
-name = input()
+name = "viktor"
 
 token = login(sock, name)
-input()
+test(sock, name, token)
+#send_msg(sock, name, token)
+get_new_msg(sock, name, token)
 quit(sock, token, name)
 
 sock.close()
 
-#sock = socket.socket()
-#sock.connect(('localhost', 5001))
-#sock.send("GET_TKN:barakuda")
-#text = sock.recv(1024).decode("utf-8")
-#print(text)
+
 
 
 
--- server/server.go
+++ server/server.go
@@ -7,6 +7,10 @@ import (
  "strings"
  "math/rand"
  "errors"
+ "strconv"
+ "encoding/binary"
+ "bytes"
+ "io"
 )
 
 const (
@@ -16,9 +20,20 @@ const (
  //---------------------
  RANDOM_TEXT_SIZE = 16
  TOKEN_SIZE = 8
+ DELEMITER = ":"
+ OK_ANSWER = "ok"
+ OK_CODE = 0
+ SERVER_HEADER_SIZE = 5
+ CLIENT_HEADER_SIZE = 4
+ ERROR_BEGIN = "?ERROR: "
 )
 
-var ServerFunctions map[string]func(net.Conn, string)error
+type feature struct {
+  f func (net.Conn, []string)error
+  cnt_args int
+}
+
+var ServerFunctions map[string]feature
 var tokens map[string]string
 var p = fmt.Println
 
@@ -41,85 +56,124 @@ func main() {
       continue
     }
     go workWithClient(conn)
+    //go echo(conn)
   }
 }
 
+func echo(conn net.Conn){
+  header := []byte("hello")
+  data, _ := recvDataB(conn)
+  fmt.Println(data)
+  conn.Write(header)
+}
+
 func initServerFunctions() {
-  ServerFunctions = make( map[string]func(net.Conn, string)error )
+  ServerFunctions = make( map[string]feature )
   tokens  = make(map[string]string)
-  ServerFunctions["GET_TKN"] = getToken
-  ServerFunctions["QUIT"] = unlogin
+  ServerFunctions["GET_TKN"] = feature{getToken, 1}
+  ServerFunctions["QUIT"] = feature{unlogin, 2}
+  ServerFunctions["SEND_MSG"] = feature{sendMsg, 4}
+  ServerFunctions["GET_MSG"] = feature{getNewMsg, 2}
+  ServerFunctions["TEST"] = feature{test, 2}
+}
+
+func sendError(conn net.Conn, errText string) {
+  fmt.Println(ERROR_BEGIN + errText)
+  conn.Write( []byte(ERROR_BEGIN + errText) )
 }
 
 func workWithClient(conn net.Conn) {
   defer conn.Close()
   fmt.Println("connected")
   for {
-    data := make([]byte, 1024)
-    data_len, err := conn.Read(data)
+    data, err := recvDataB(conn)
+    if err != nil {
+      if err == io.EOF {
+        break
+      }
+      sendError(conn, err.Error())
+      continue
+    }
+
+    req := strings.Split(string(data[:len(data)]), DELEMITER)
+    cmd := req[0]
+    args := req[1:]
+
+    ficha, ok := ServerFunctions[cmd]
+    if !ok {
+      sendError(conn, "wrong request cmd")
+      continue
+    }
+    if len(args) != ficha.cnt_args {
+      sendError(conn, "wrong count arguments in " + cmd + ". Expected " +
+                  strconv.Itoa(ficha.cnt_args) +  ", got " + strconv.Itoa(len(args)) )
+      continue
+    }
+    // Run comand
+    err = ficha.f(conn, args)
 
     if err != nil {
-      fmt.Println("Error reading:", err.Error())
-      break
+      if err == io.EOF {
+        break
+      }
+      sendError(conn, err.Error())
+    } else {
+      sendOkStatus(conn)
     }
-    if data_len == 0 {
-      fmt.Println("No data")
-      break
-    }
-
-    text := string(data[:data_len])
-    cmd := strings.Split(text, ":")[0]
-    args := text[len(cmd)+1:]
-    err = ServerFunctions[cmd](conn, args)
-    p(tokens)
-    if err != nil {
-      break
-    }
-
-
   }
 }
 
+/////////////////////////////////////////
 
-func getToken(conn net.Conn, args string) error {
-
+func getToken(conn net.Conn, args []string) error {
   inText := getRandomText(RANDOM_TEXT_SIZE)
   inData := []byte(inText)
   // ENCRYPT inData || inText
-  conn.Write(inData)
+  sendDataB(conn, inData, uint32(len(inData)), OK_CODE)
 
-  ans := make([]byte, len(inData))
-  _, err := conn.Read(ans)
+  ans, err := recvDataB(conn)
 
   if err != nil {
-    fmt.Printf("Error reading in getToken\n")
-    return errors.New("Error reading in getToken\n")
+    return err
+    //return errors.New("reading in getToken")
   }
 
   if inText != string(ans) {
-      fmt.Printf("Error: wrong decrypted random_text\n")
-      return errors.New("Error: wrong decrypted random_text\n")
+      return errors.New("wrong decrypted random_text")
   }
 
   bToken := []byte( getRandomText(TOKEN_SIZE) )
-  conn.Write(bToken)
+  sendDataB(conn, bToken, uint32(len(bToken)), OK_CODE)
 
 
-  tokens[args] = string(bToken)
+  tokens[args[0]] = string(bToken)
 
   return nil
 }
 
-func unlogin(conn net.Conn, args string) error {
-    token := strings.Split(args, ";")[0]
-    login := args[len(token)+1:]
+func unlogin(conn net.Conn, args []string) error {
+    token := args[0]
+    login := args[1]
 
     if tokens[login] == token {
       delete(tokens, login)
     }
-
     return nil
 }
+
+func test(conn net.Conn, args []string) error {
+  return nil
+}
+
+func sendMsg(conn net.Conn, args []string) error {
+  return nil
+}
+
+func getNewMsg(conn net.Conn, args []string) error {
+  return nil
+}
+
+///////////////////////////////////////
 
 func getRandomText(text_len int) string {
   // without ';' and ':'
@@ -132,6 +186,45 @@ func getRandomText(text_len int) string {
   return res
 }
 
+func sendOkStatus(conn net.Conn) {
+  sendData(conn, OK_ANSWER, uint32(len(OK_ANSWER)), OK_CODE)
+}
+
+func sendDataB(conn net.Conn, data []byte, dataSize uint32, code uint8) {
+  header := make([]byte, SERVER_HEADER_SIZE)
+  binary.LittleEndian.PutUint32(header, dataSize)
+  header[4] = code
+  res := append(header, data...)
+  conn.Write(res)
+}
+
+func sendData(conn net.Conn, textData string, dataSize uint32, code uint8) {
+  sendDataB(conn, []byte(textData), dataSize, code)
+}
+
+func recvDataB(conn net.Conn) ([]byte, error) {
+  header := make( []byte, CLIENT_HEADER_SIZE)
+  data_len, err := conn.Read(header)
+
+  if err != nil {
+    return nil, err
+  }
+
+  var dataSize uint32
+  binary.Read(bytes.NewReader(header[0:4]), binary.LittleEndian, &dataSize)
+
+  data := make([]byte, dataSize)
+  data_len, err = conn.Read(data)
+
+  if err != nil {
+    return nil, err
+  }
+  if data_len != int(dataSize) {
+    return nil, errors.New("real and expected data size not equal")
+  }
+
+  return data, nil
+}
 
 
 
